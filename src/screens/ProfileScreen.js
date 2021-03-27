758e0130c808a884bde578c6f470bd75acb9595b
Tri <nguyenhuutri1620vn@gmail.com>
2021-03-27 20:30:34 +0700

    first commit

---
+++ src/screens/ProfileScreen.js
@@ -0,0 +1,103 @@
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+import {StyleSheet, View, Text, StatusBar, ScrollView, TouchableOpacity} from 'react-native';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { createStackNavigator } from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import 'react-native-gesture-handler';
+const Stack = createStackNavigator();
+const ProfileItem = ({icon, name}) => (
+  <View style={styles.itemContainer}>
+    <Text style={[styles.itemText, {marginLeft: icon ? 20 : 0}]}>{name}</Text>
+  </View>
+);
+const ProfileScreen=({navigation}) =>{
+  return (
+    <ScrollView>
+    <View style={styles.screenContainer}>
+      <View style={styles.bodyContainer}>
+        <TouchableOpacity onPress={()=>navigation.navigate(Login)}>
+        <View style={styles.userContainer}>
+          <View style={styles.avatarContainer}>
+          </View>
+          <View style={styles.textContainer}>
+            <Text style={styles.welcomeText}>Welcome to Iphone store</Text>
+            <Text style={styles.authText}>Login/Sign up</Text>
+          </View>
+        </View>
+        </TouchableOpacity>
+        {/*  */}
+        <View style={styles.divider} />
+        <ProfileItem icon="format-list-bulleted" name="Quản lý đơn hàng" />
+        <ProfileItem icon="cart-outline" name="Sản phẩm đã mua" />
+        <ProfileItem icon="heart-outline" name="Sản phẩm yêu thích" />
+        <ProfileItem icon="star-outline" name="Đổi mật khẩu" />
+        {/*  */}
+        <View style={styles.divider} />
+        <ProfileItem icon="star-outline" name="Ưu đãi cho chủ thẻ ngân hàng" />
+        <ProfileItem icon="star-outline" name="Cài đặt" />
+        {/*  */}
+        <View style={styles.divider} />
+        <ProfileItem icon="headphones" name="Hỗ trợ" />
+      </View>
+    </View>
+    </ScrollView>
+  );
+};
+const styles = StyleSheet.create({
+  screenContainer: {
+    flex: 1,
+  },
+  bodyContainer: {
+    flex: 1,
+    backgroundColor: '#ededed',
+  },
+  //
+  userContainer: {
+    backgroundColor: '#fff',
+    flexDirection: 'row',
+    paddingHorizontal: 20,
+    paddingVertical: 22,
+    alignItems: 'center',
+  },
+  avatarContainer: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#1e88e5',
+  },
+  textContainer: {
+    flex: 1,
+    marginLeft: 20,
+  },
+  welcomeText: {
+    color: '#828282',
+  },
+  authText: {
+    color: 'black',
+    fontSize: 18,
+    fontWeight: '500',
+  },
+  //
+  itemContainer: {
+    backgroundColor: '#fff',
+    flexDirection: 'row',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    alignItems: 'center',
+  },
+  itemText: {
+    flex: 1,
+    color: '#1e1e1e',
+  },
+  //
+  divider: {
+    height: 10,
+  },
+ 
+});
+
+export default ProfileScreen;
---
+++ src/screens/Signup.js
@@ -0,0 +1,122 @@
+import React, {Component} from 'react';
+import {Login} from './Login';
+import {View, Text, TouchableOpacity, TextInput, TouchableWithoutFeedback, Keyboard,StyleSheet, AppRegistry, Alert} from 'react-native';
+export default class Signup extends Component {
+    constructor(props){
+        super(props);
+        this.state={email:'', password:''}   
+    }
+    render(){
+        const {navigation} = this.props;
+    return(
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <View style={styles.container}>
+                <View style={styles.down}>
+                <View style={styles.txtipcontainer}>
+                    <TextInput style={styles.txtInput}
+                    placeholder="Enter your email"
+                    textContentType='emailAddress'
+                    keyboardType='email-address'
+                    ></TextInput>
+                </View>
+                <View style={styles.txtipcontainer}>
+                    <TextInput style={styles.txtInput}
+                    placeholder="Enter your password"
+                    secureTextEntry={true}
+                    ></TextInput>
+                </View>
+                <TouchableOpacity 
+                style={styles.loginbtn} 
+                >
+                    <Text style ={styles.loginbtntitle}>Register</Text>
+                </TouchableOpacity>
+                <View style={styles.signuptxt}>
+                    <Text style={styles.txtsg}>Already have an account ? Get </Text>
+                    <Text style={styles.lbsg} onPress={()=>navigation.navigate(Login)}>Login</Text>
+                </View>
+                </View>
+            </View>
+            </TouchableWithoutFeedback>
+    )
+    }
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'stretch',
+        backgroundColor: 'rgb(234, 195, 176)'
+    },
+    up:{
+        flex:4,
+        flexDirection: 'column',
+        backgroundColor: '#89cff0',
+        alignItems:'center',
+        alignContent: 'center',
+        justifyContent: 'center',
+  
+    },
+    down:{
+        flex:7,
+        flexDirection: 'column',
+        backgroundColor: '#89cff0',
+        alignItems:'center',
+        justifyContent:'flex-start',
+    },
+    title:{
+        color:'white',
+        textAlign:'center',
+        fontSize:18,
+        width: 400,
+        marginTop:10,      
+    },
+    txt:{
+        fontSize:23,
+        color:'#89cff0',
+        width:400,
+        textAlign:'center',
+        fontWeight: 'bold',
+    },
+    txtipcontainer:{
+        paddingHorizontal: 10,
+        backgroundColor: 'rgba(255,255,255,0.5)',
+        borderRadius: 25,
+        marginTop: 20,
+    },
+    txtInput:{
+        width:280,
+        height:45,
+        borderRadius: 25
+    },
+    loginbtn:{
+        width:300,
+        height:45,
+        borderRadius: 6,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor:'#0492c2',
+        marginTop: 20,
+        borderRadius: 25
+    },
+    loginbtntitle:{
+        fontSize: 18,
+        color: 'white',
+    },
+    signuptxt:{
+        flex:3,
+        alignItems: 'flex-end',
+        marginVertical: 16,
+        justifyContent:'center',
+        flexDirection:'row'
+    },
+    txtsg:{
+        color: '#0492c2'
+    },
+    lbsg:{
+        color:'white',
+        fontSize: 16,
+  
+    }
+  })
